db8377d43d1e083335837432493ec97555150af1
Hugo Fabricio <hugo.fabricio@superlogica.com>
2020-01-02 17:28:24 -0300

    Init adonis project

---
+++ app/Models/User.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Model = use('Model')
+const Hash = use('Hash')
+
+class User extends Model {
+  static boot () {
+    super.boot()
+
+    /**
+     * A hook to hash the user password before saving
+     * it to the database.
+     */
+    this.addHook('beforeSave', async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password)
+      }
+    })
+  }
+
+  tokens () {
+    return this.hasMany('App/Models/Token')
+  }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task')
+  }
+}
+
+module.exports = User
